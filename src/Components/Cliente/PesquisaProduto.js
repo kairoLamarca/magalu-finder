ffbae64ba774276d13f9c63180c2a4dc9bf73bbc
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-10-28 19:50:19 -0300

    Implementado pesquisa por descrição do produto na área do cliente
    Implementado pesquisa por descrição do produto na área do cliente, e preenchendo a tabela com os registros encontrados

--- src/Components/Cliente/PesquisaProduto.js
+++ src/Components/Cliente/PesquisaProduto.js
@@ -34,9 +34,14 @@ class PesquisaProduto extends Component {
 
                 await this.setState({ produtosEncontrados: response.data });
             }
+            else if (this.state.descricao && this.state.cep) {
+                response = await axios.get(`http://localhost:4000/cliente/produtoloja/descricao/cep/${this.state.descricao}/${this.state.cep}`);
+
+                await this.setState({ produtosEncontrados: response.data });
+            }
             else {
                 response = await axios.get(`http://localhost:4000/cliente/produtoloja/`);
-                
+
                 await this.setState({ produtosEncontrados: response.data });
             }
         } catch (error) {
@@ -91,7 +96,7 @@ class ListarProdutosLojas extends Component {
                     <tr>
                         <th>Código do Produto</th>
                         <th>Produto</th>
-                        <th>Distância</th>                        
+                        <th>Distância</th>
                         <th>Descrição</th>
                         <th>CEP</th>
                     </tr>
